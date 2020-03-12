463f5d71475dae8dabb99d975ab22d4e4aa80989
PauloNayron <paulonayron.s@hotmail.com>
2020-03-11 23:54:22 -0300

    Erro de inferencia de Variáveis

---
+++ src/fundamentos/CuidadoInferencia1.kt
@@ -0,0 +1,15 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+     /*
+     o kotlin precisa que seja atribuido o tipo, ou que a variável seja inicializada
+     de forma que a linha abaixo gera erro
+     */
+//    var a
+    var a: Int
+    var b = 2
+
+    a = 10
+
+    print(a+b)
+}
