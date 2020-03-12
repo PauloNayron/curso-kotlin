66bd4bf1786b201b6028cb373d78f8da0afc7f60
PauloNayron <paulonayron.s@hotmail.com>
2020-03-11 23:57:36 -0300

    Mudança de tipos em kotlin

---
+++ src/fundamentos/CuidadoInferencia2.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    var a = 1
+    var b: Double = 2.0
+    /*
+    * O Kotlin Não perte que a mudança de tipos */
+//    a = 2.3  // gera erro
+    b = 2.3
+}
